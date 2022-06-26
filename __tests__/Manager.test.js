774cf9cd4a46199a730d1b3d0a0cc285d8d73ea4
Olive <olivelliott48@gmail.com>
2022-06-26 17:20:32 -0400

    Update manager testing methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,15 +1,23 @@
 const Manager = require("../lib/Manager.js");
 
 
-describe('OfficeNumber', () => {
-    const manager = new Manager('Olive', 10, 'example@gmail.com', 123, 'manager');
+describe("Manager Profile", () => {
+  it("Checks to see if office number value is retrieved", () => {
+    const officeNumber = 10;
+    const newManager = new Manager("Olive", 10, "example@gmail.com", officeNumber);
+    expect(newManager.officeNumber).toBe(officeNumber);
+  });
 
-    it('checks if the office number is a number', () => {
-        expect(manager.officeNumber).toEqual(expect.any(Number));
-    })
+  it("Checks the getofficeNumber method to return the office number value", () => {
+    const officeNumber = 5;
+    const newManager = new Manager("Olive", 10, "example@gmail.com", officeNumber);
+    expect(newManager.getOfficeNumber()).toEqual(officeNumber);
+  });
 
-      it("Checks to ensure the correct role is assigned (manager)", () => {
-        expect(manager.role).toEqual("manager");
-      });
-
+  it("Checks the role method to return as Manager", () => {
+    const returnRole = "Manager";
+    const newManager = new Manager("Olive", 10, "example@gmail.com", 5);
+    expect(newManager.getRole()).toBe(returnRole);
+  });
+  
 });
