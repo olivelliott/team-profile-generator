0d3557f272a3bf1e60ee1a5c746bd7ec18f93b3c
Olive <olivelliott48@gmail.com>
2022-06-26 18:58:05 -0400

    Fix spacing issues & add sample-index.html

--- src/profile-template.js
+++ src/profile-template.js
@@ -2,55 +2,55 @@ const generateTeam = team => {
 
     const generateManager = manager => {
         return `
-            <div class="card employee-card rounded col col-lg-3 mx-auto">
-                <div class="card-header bg-dark text-white">
-                    <h2 class="card-title">${manager.getName()}</h2>
-                    <h3 class="card-title"><i class="icon fa-solid fa-star fa-xs"></i>${manager.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${manager.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                        <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                    </ul>
-                </div>
+        <div class="card employee-card rounded col col-lg-3 mx-auto">
+            <div class="card-header bg-dark text-white">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="icon fa-solid fa-star fa-xs"></i>${manager.getRole()}</h3>
             </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
         `;
     }
 
     const generateEngineer = engineer => {
         return `
-            <div class="card employee-card rounded col col-lg-3 mx-auto">
-                <div class="card-header bg-dark text-white">
-                    <h2 class="card-title">${engineer.getName()}</h2>
-                    <h3 class="card-title"><i class="fa-solid fa-glasses fa-xs"></i>${engineer.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${engineer.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
-                    </ul>
-                </div>
+        <div class="card employee-card rounded col col-lg-3 mx-auto">
+            <div class="card-header bg-dark text-white">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fa-solid fa-glasses fa-xs"></i>${engineer.getRole()}</h3>
             </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
         `;
     };
 
     const generateIntern = intern => {
         return `
-            <div class="card employee-card rounded col col-lg-3 mx-auto">
-                <div class="card-header bg-dark text-white">
-                    <h2 class="card-title">${intern.getName()}</h2>
-                    <h3 class="card-title"><i class="fa-solid fa-graduation-cap fa-xs"></i>${intern.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${intern.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                        <li class="list-group-item">School: ${intern.getSchool()}</li>
-                    </ul>
-                </div>
+        <div class="card employee-card rounded col col-lg-3 mx-auto">
+            <div class="card-header bg-dark text-white">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fa-solid fa-graduation-cap fa-xs"></i>${intern.getRole()}</h3>
             </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
         `;
     };
 
@@ -80,8 +80,8 @@ const generateTeam = team => {
 
 module.exports = team => {
     return `
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -109,6 +109,6 @@ module.exports = team => {
             ${generateTeam(team)}
         </div>
     </body>
-    </html>
+</html>
     `;
 };
